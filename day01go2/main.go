559ff43c88bfb4357b062a43771fa8f5831bddde
Andzhey Asryan <andzhey.asryan@freiheit.com>
2022-12-01 12:51:36 +0100

    Day01b

---
+++ day01go2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	"advents2022/util"
+)
+
+func main() {
+	input := util.ReadFile("./day01go1/input.txt")
+	input = append(input, "")
+	fmt.Println(topThree(input))
+}
+
+func topThree(ss []string) int {
+	calories := make([]int, 10)
+	var sum int
+	for _, s := range ss {
+		sum += util.ToInt(s)
+		if s == "" {
+			calories = append(calories, sum)
+			sum = 0
+		}
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(calories)))
+	for i := 0; i < 3; i++ {
+		sum += calories[i]
+	}
+	return sum
+}
---
+++ day01go2/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+
+	"advents2022/util"
+)
+
+func Test_code(t *testing.T) {
+	for _, test := range []struct {
+		in   string
+		want int
+	}{
+		{
+			in: `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+
+`,
+			want: 45000,
+		},
+	} {
+		t.Run(test.in, func(t *testing.T) {
+			if got := topThree(util.ReadString(test.in)); got != test.want {
+				t.Errorf("got = %d, want %d", got, test.want)
+			}
+		})
+	}
+}
